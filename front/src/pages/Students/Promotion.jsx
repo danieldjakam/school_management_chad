b38f6b031b5d5a1fd40552e36f54c2d87727e2f1
danieldjakam <86420544+danieldjakam@users.noreply.github.com>
2022-09-25 14:29:55 +0100

    make the view for promotion;

--- front/src/pages/Students/Promotion.jsx
+++ front/src/pages/Students/Promotion.jsx
@@ -10,8 +10,8 @@ import ReactLoading from 'react-loading';
 const Promotion = () => {
     const [students, setStudents ] = useState([]);
     const [subjects, setSubjects ] = useState([]);
+    const [classes, setClasses] = useState([]);
     const [ActualClass, setActualClass ] = useState({});
-    const [actualExam, setActualExam ] = useState({});
     const [loading, setLoading ] = useState(false);
     const {exam_id, class_id, type} = useParams();
     const [notes, setNotes] = useState({});
@@ -23,11 +23,10 @@ const Promotion = () => {
                     'Authorization': sessionStorage.user
                 }})
                 const dat = await re.json();
-                const resp = await fetch(host+'/annuals/'+exam_id, {headers: {
+                const resp = await fetch(host+'/classes/by-level/'+dat.level, {headers: {
                     'Authorization': sessionStorage.user
                 }})
                 const data = await resp.json();
-                console.log(data);
                 const resp2 = await fetch(host+'/class/'+class_id, {headers: {
                     'Authorization': sessionStorage.user
                   }})
@@ -41,8 +40,8 @@ const Promotion = () => {
                 }})
                 const data5 = await resp5.json();
                 
+                setClasses(data);
                 setStudents(dat);
-                setActualExam(data);
                 setActualClass(data2);
                 setSubjects(data4);
                 setNotes(data5);
@@ -83,15 +82,20 @@ const Promotion = () => {
                                         herNotes.forEach(no => {
                                             total += parseInt(no.value);
                                         })
+                                        const diviser = parseInt(type) === 1 ? 
+                                                                            subjects.length * 10
+                                                                        :  subjects.length * 20;
+                                        let average = Math.round((total / diviser) * 20 * 100) / 100;
+                                        
                                         return <tr key={id}>
                                             <td>{id + 1}</td>
                                             <td>{student.name}</td>
                                             <td>{student.subname}</td>
                                             <td>{student.sex === 'm' ? studentTraductions[getLang()].m : studentTraductions[getLang()].f}</td>
                                             <td>
-                                                {
-                                                    
-                                                }
+                                                {`
+                                                    ${average} / 20
+                                                `}
                                             </td>
                                         </tr> 
                                     }) : <tr> 
